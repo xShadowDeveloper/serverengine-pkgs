fbfa17f22d41d6e3440bb365ade12d2b4bbd5200
xShadowDeveloper <106242550+xShadowDeveloper@users.noreply.github.com>
2025-10-04 13:42:05 +0200

    Sync upload SEExample_StoreVariables/Step2_Load-Variable.ps1

---
+++ SEExample_StoreVariables/Step2_Load-Variable.ps1
@@ -0,0 +1,9 @@
+# To access the stored variables
+# load it like this: $message = $store
+
+#load variable from $store
+$message = $store
+
+#output loaded variable in logs
+Write-Host "<WRITE-LOG = ""*load: $message*"">"
+write-host $message
