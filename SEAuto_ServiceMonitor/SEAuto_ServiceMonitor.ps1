9fef157a9d3de864ce465142dd6a2f6abf4753cd
xShadowDeveloper <106242550+xShadowDeveloper@users.noreply.github.com>
2025-10-05 19:13:32 +0200

    Sync upload SEAuto_ServiceMonitor/SEAuto_ServiceMonitor.ps1

--- SEAuto_ServiceMonitor/SEAuto_ServiceMonitor.ps1
+++ SEAuto_ServiceMonitor/SEAuto_ServiceMonitor.ps1
@@ -16,6 +16,13 @@ The above copyright notice and this permission notice shall be included in all c
 or substantial portions of the Script.
 #>
 
+$isadm = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if (-not $isadm) {
+    Write-Host "<WRITE-LOG = ""*Please run this script as Administrator.*"">"
+    Write-Error "Warning: Not running as Administrator."
+    exit 1 
+}
+
 $CriticalServices = @(
     "Spooler",
     "wuauserv",
